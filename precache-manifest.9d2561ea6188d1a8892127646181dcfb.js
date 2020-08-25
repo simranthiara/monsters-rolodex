6e031e5cc370c242e0a958632dd9f94439af18cd
Simran Thiara <simranthiara@Simrans-MacBook-Air.local>
2020-08-25 02:17:00 -0700

    Updates

---
+++ precache-manifest.9d2561ea6188d1a8892127646181dcfb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0783df842194f2cffef08527bfa2bff2",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "24ce68b5e7e86b3e2512",
+    "url": "/monsters-rolodex/static/css/main.3959a285.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24ce68b5e7e86b3e2512",
+    "url": "/monsters-rolodex/static/js/main.9101924f.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
